2add016534419fae816e552ca0b047b68ccc6c4e
zluo16 <zluo@alumni.berklee.edu>
2017-06-05 11:22:02 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!!! I BUILT THIS!!!\nI started my server with shotgun this time!!!"
   end
 
 end
